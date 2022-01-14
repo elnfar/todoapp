075d008d36915e930063dcf04c24396dc1c9318c
Elnur Farzaliyev <farzaliyevelnur00@gmail.com>
2022-01-14 21:06:14 +0400

    Enter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Form from './Form';
+import TodoList from './TodoList';
+
 
 function App() {
+
+  const [inputText,setInputText] = useState('');
+  const [todos,setTodos] = useState([])
+
+  const [option,setOption] = useState('all')
+  const [changer,setChanger] = useState([]);
+
+
+  useEffect(() => {
+    getFromLocal()
+  },[])
+
+
+  useEffect(() => {
+    changeOptions()
+    saveToLocal();
+  },[todos,option])
+
+
+  const changeOptions = () => {
+    switch(option) {
+      case 'finished':
+        setChanger(todos.filter(todo => {
+          return todo.completed === true
+        }))
+        break;
+
+        case 'uncompleted':
+          setChanger(todos.filter(todo => {
+            return todo.completed === false
+          }))
+          break;
+          default:
+            setChanger(todos)
+            break
+    }
+  }
+
+const saveToLocal = () => {
+  localStorage.setItem("todos",JSON.stringify(todos))
+}
+
+const getFromLocal = () => {
+  if(localStorage.getItem("todos") === null) {
+    localStorage.setItem("todos",JSON.stringify([]));
+  }else {
+    let local = JSON.parse(localStorage.getItem("todos"))
+    setTodos(local);
+  }
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} option={option} setOption={setOption}/>
+      <TodoList todos={todos} setTodos={setTodos} changer={changer}/>
     </div>
   );
 }
